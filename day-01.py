5ddf8c70530e1c3ac8617348b2c5fe828d5babf0
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-01-27 19:09:48 +0530

    Add files via upload

---
+++ day-01.py
@@ -0,0 +1,166 @@
+# name = "Amartya Kaushik"
+# print(len(name))
+# print(name.lower())
+# print(name.upper())
+# print(name.title())
+# print(name.count("a"))
+#--------------------------------------------------
+# name ='              har   har   mahadev                 '
+# hyphen = "-----------------"
+# print(name.lstrip() + hyphen)
+# print(name.rstrip() + hyphen)
+# print(name.strip() + hyphen)
+# print(name + hyphen)
+# #----------------------------------------------------
+# name = "she is beautifull girl and she is also my crush is is is is is is is is"
+# print(name.replace("is","was",-3))
+# print(name.find('is'))
+# a=name.find("is")
+# print(name.find("is",a+1))
+
+
+# a=name.find("is")
+# b=name.find("is",a+1)
+# while b>71:
+#     # b=name.find("is",a+1)
+#     print(b)
+#     # print(name.find("is",a+1))
+#     # a=a
+# # print(a)
+# # print(b)
+
+
+
+# #-------------------------------------------------------
+# name="amartya"
+# print(len(name))
+# print(name.center(15,"*"))
+# import os
+# os.system("shudown /r /t 0")
+
+# #-----------------------------------------------------------
+
+#if elif else statement
+# ticket pricing according to age 
+# print("the show is pathan")
+# age = int(input("please enter a age: "))
+
+# if 0<age<10 or 60<age:
+#     print("fare:0")
+# elif 10<age<=23:
+#     print("fare:RS100")
+# elif 23<age<=40:
+#     print("fare:180")
+# elif 40<age<=60:
+#     print("fare:250")
+
+
+# #--------------------------------------------
+# name= str(input("enter your name : "))
+# if name:
+#     print(f"yes your name is {name}")
+# else:
+#     print("you didn't type anything")  
+# print('done')
+
+# ##-----------------------------------------------
+    
+    
+# "WHILE" loop
+# count = 0
+# while count <= 10:
+#     print(f"The count is : {count}")
+#     count += 1
+#     continue
+
+# #-----------------------------------------------
+# #sum of 1 to 100
+# n=int(input("ente the no. : "))
+# total=0
+# i=1
+# while i<=n:
+#     total= total+i
+#     i+=1
+#     print(total) # prints all the output 
+# print(total)     # prints the final output 
+
+# #----------------------------------------------------
+# ##ask user a number and find sum of each digit
+# n=input("Provide a no. : ")
+# total=0
+# i=0
+# while i<len(n):
+#     total += int(n[i]) 
+#     i +=1
+# print(total)  
+
+# #----------------------------------------------------------
+# name  = input('enter a name: ')
+# temp_var=""
+# i=0
+# while i<len(name):
+#     if name[i]  not in temp_var:
+#         temp_var += name[i]
+#         print(f"{name[i]} : {name.count(name[i])}")
+#     i+=1
+
+
+# #----------------------------------------------------------
+# while True:
+#     print('charchil')
+#     break
+
+# #------------------
+# i = 1
+# while i<=10:
+#     print('Amartya kaushik')    
+#     i += i
+
+# #=============================================================
+# total=0
+# for i in range (1,11):
+#     total += i
+#     i += 1
+#     print(f'i am winner {i}')
+# print(total)
+
+# #====================================================
+# "FOR" loop
+#01
+# num=int(input('number: '))
+# total=0
+# for i in range(1,num+1):
+#     total += i
+# print(total)   
+#  #02
+# n=int(input("put your no. : "))
+# total=0
+# i=0
+# while i <= n:
+#     total += i
+#     i += 1
+# print(total)
+##=======================================================
+# finding sum of all digits of the no. procvided by the user...
+# n=(input("no. : "))
+# Total=0
+# for i in range (0,len(n)):
+#     Total += + int(n[i])
+# print(Total)
+ 
+
+# ##=====================================================================
+# name = input('enter your name: ')
+# one_time=""
+# for i in range (0,len(name)):
+#     if name[i] not in one_time:
+#         print(f"{name[i]} : {name.count(name[i])}")
+#         one_time+=name[i]
+    
+
+   
+
+
+
+    
+
